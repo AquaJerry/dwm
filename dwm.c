6346956f26ed8a47a8b38ca49efa77fad3a0868a
aquajerry <huopopo_zeyang@163.com>
2020-06-28 07:59:16 +0000

    simplify: disable default dwm-VERSION status text

--- dwm.c
+++ dwm.c
@@ -1954,8 +1954,7 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
-	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
+	gettextprop(root, XA_WM_NAME, stext, sizeof(stext));
 	drawbar(selmon);
 }
 
